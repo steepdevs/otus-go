2270f1ee09c6b98f684374fb806c67a50d572726
Artur Turchin <a.turchin@artox.com>
2020-11-21 13:27:08 +0300

    feat(escaping): allow escape digits, backslashes with backslash
    Allow unpack next examples:
    qwe\4\5 => qwe45
    qwe\45 => qw44444
    qwe\\5 => qwe\\\\\
    \1a2b3 => 1aabbb
    \34 => 3333
    
    hw-1

--- hw-2/unpack.go
+++ hw-2/unpack.go
@@ -14,28 +14,49 @@ func Unpack(s string) (string, error) {
 
 	result, char := "", ""
 	runes := []rune(s)
+	isEscapedChar := false
+	repeat := 0
 
 	for i, r := range runes {
+		if !isEscapedChar && string(r) == "\\" {
+			isEscapedChar = true
+			continue
+		}
+
 		if unicode.IsDigit(r) {
 			if i == 0 {
 				return "", fmt.Errorf("string has to start from character, not a number")
-			} else if unicode.IsDigit(runes[i-1]) {
+			} else if (i + 1) < len(runes) && !isEscapedChar && unicode.IsDigit(runes[i+1]) {
 				return "", fmt.Errorf("string can't contains numbers, you have to use only digits")
 			}
 
-			repeat, err := strconv.Atoi(string(r))
+			if isEscapedChar {
+				char = string(r)
+				isEscapedChar = false
+			} else {
+				digit, err := strconv.Atoi(string(r))
 
-			if err != nil {
-				return "", fmt.Errorf("cannot parse letter: %v", r)
+				if err != nil {
+					return "", fmt.Errorf("cannot parse letter: %v", r)
+				}
+
+				repeat = digit
 			}
+		} else {
+			isEscapedChar = false
+			char = string(r)
+		}
 
+		// Check if char not repeated:
+		// 1. end of string (ex: a3b2c)
+		// 2. next char is string (ex: ab3)
+		if repeat == 0 && ((i + 1) == len(runes) || !unicode.IsDigit(runes[i+1])) {
+			repeat = 1
+		}
+
+		if char != "" && repeat > 0 {
 			result += strings.Repeat(char, repeat)
-		 } else {
-		 	char = string(r)
-
-		 	if (i + 1) == len(runes) || !unicode.IsDigit(runes[i+1]) {
-		 		result += char
-			}
+			repeat = 0
 		}
 	}
 
--- hw-2/unpack_test.go
+++ hw-2/unpack_test.go
@@ -10,25 +10,74 @@ func TestUnpackUsualCase(t *testing.T) {
 	s := "a2b3cd4"
 	actual, err := Unpack(s)
 
-	require.Equal(t, "aabbbcdddd", actual, fmt.Sprintf("unpacking string: %s", s))
 	require.Nil(t, err)
+	require.Equal(t, "aabbbcdddd", actual, fmt.Sprintf("unpacking string: %s", s))
 }
 
 func TestUnpackSimplestCase(t *testing.T) {
 	s := "abcd"
 	actual, err := Unpack(s)
 
-	require.Equal(t, "abcd", actual, fmt.Sprintf("unpacking string: %s", s))
 	require.Nil(t, err)
+	require.Equal(t, "abcd", actual, fmt.Sprintf("unpacking string: %s", s))
 }
 
 func TestUnpackEmptyString(t *testing.T) {
 	s := ""
 	actual, err := Unpack(s)
 
-	require.Equal(t, "", actual, fmt.Sprintf("unpacking string: %s", s))
 	require.Nil(t, err)
+	require.Equal(t, "", actual, fmt.Sprintf("unpacking string: %s", s))
 }
+
+func TestUnpackStringContainsEscapedDigits(t *testing.T) {
+	s := `abcd\5`
+	actual, err := Unpack(s)
+
+	require.Nil(t, err)
+	require.Equal(t, "abcd5", actual, fmt.Sprintf("unpacking string: %s", s))
+}
+
+func TestUnpackUsualCaseContainsEscapedDigits(t *testing.T) {
+	s := `\12a2b3\53c\3d4`
+	actual, err := Unpack(s)
+
+	require.Nil(t, err)
+	require.Equal(t, "11aabbb555c3dddd", actual, fmt.Sprintf("unpacking string: %s", s))
+}
+
+func TestUnpackUsualCaseContainsEscapedBackslash(t *testing.T) {
+	s := `\\a2\\2b3\53c\3d4\\\`
+	actual, err := Unpack(s)
+
+	require.Nil(t, err)
+	require.Equal(t, `\aa\\bbb555c3dddd\`, actual, fmt.Sprintf("unpacking string: %s", s))
+}
+
+func TestUnpackTask1(t *testing.T) {
+	s := `qwe\4\5`
+	actual, err := Unpack(s)
+
+	require.Nil(t, err)
+	require.Equal(t, `qwe45`, actual, fmt.Sprintf("unpacking string: %s", s))
+}
+
+func TestUnpackTask2(t *testing.T) {
+	s := `qwe\45`
+	actual, err := Unpack(s)
+
+	require.Nil(t, err)
+	require.Equal(t, `qwe44444`, actual, fmt.Sprintf("unpacking string: %s", s))
+}
+
+func TestUnpackTask3(t *testing.T) {
+	s := `qwe\\5`
+	actual, err := Unpack(s)
+
+	require.Nil(t, err)
+	require.Equal(t, `qwe\\\\\`, actual, fmt.Sprintf("unpacking string: %s", s))
+}
+
 
 func TestUnpackStringStartsWithDigit(t *testing.T) {
 	s := "1abcd"
@@ -40,6 +89,14 @@ func TestUnpackStringStartsWithDigit(t *testing.T) {
 
 func TestUnpackStringContainsNumbers(t *testing.T) {
 	s := "a1bc48d"
+	actual, err := Unpack(s)
+
+	require.EqualError(t, err, "string can't contains numbers, you have to use only digits", fmt.Sprintf("unpacking string: %s", s))
+	require.Equal(t, "", actual)
+}
+
+func TestUnpackStringContainsNumbersAndEscapedBackslash(t *testing.T) {
+	s := `a1bc\\48d`
 	actual, err := Unpack(s)
 
 	require.EqualError(t, err, "string can't contains numbers, you have to use only digits", fmt.Sprintf("unpacking string: %s", s))
@@ -67,5 +124,11 @@ func BenchmarkUnpackWithError(b *testing.B) {
 func BenchmarkUnpackEmptyString(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		_, _ = Unpack("")
+	}
+}
+
+func BenchmarkUnpackHeavyString(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, _ = Unpack(`\\a2\\2b3\53c\3d4\\\`)
 	}
 }
